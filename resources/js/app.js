a3b79314917b33eaa6869d646501b36b33307397
Steven Howard <steven@smhoward.net>
2018-03-29 07:16:07 -0400

    version 0.5

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,7 +11,35 @@
 */
 
 // VARIABLE DECLARATION
-let   winningWord = "ZOO",
+let winningWord = "ZOO",
+      winWhenZeroLength = [],
+      guessedWrong,
       guessesLeft = 8,
-      doneWhenEmpty = [],
       guessedAlready = [];
+
+// UI ELEMENTS
+
+const game = document.querySelector('#game'),
+      letter0 = document.querySelector('#letter-0'),
+      letter1 = document.querySelector('#letter-1'),
+      letter2 = document.querySelector('#letter-2'),
+      guessedWrong0 = document.querySelector('btn-guessed-wr-0'),
+      guessedWrong1 = document.querySelector('btn-guessed-wr-1'),
+      guessedWrong2 = document.querySelector('btn-guessed-wr-2'),
+      guessedWrong3 = document.querySelector('btn-guessed-wr-3'),
+      guessedWrong4 = document.querySelector('btn-guessed-wr-4'),
+      guessedWrong5 = document.querySelector('btn-guessed-wr-5'),
+      guessedWrong6 = document.querySelector('btn-guessed-wr-6'),
+      guessedWrong7 = document.querySelector('btn-guessed-wr-7'),
+      emmetHead01 = document.querySelector('#01-head'),
+      emmetTorso02 = document.querySelector('#02-torso'),
+      emmetRtArm03 = document.querySelector('#03-rt-arm'),
+      emmetLftArm04 = document.querySelector('#04-lft-arm'),
+      emmetRtLeg05 = document.querySelector('#05-rt-leg'),
+      emmetLftLeg06 = document.querySelector('#06-lft-leg'),
+      emmetRtFoot07 = document.querySelector('#07-rt-foot'),
+      emmetLftFoot08 = document.querySelector('#08-lft-foot'),
+      guessBtn = document.querySelector('#guess-btn'),
+      guessInput = document.querySelector('#guess-input'),
+      listMsg = document.querySelector('#special-message'),
+      resetBtn = document.querySelector('#reset-btn');
